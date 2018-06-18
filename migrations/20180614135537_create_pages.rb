366e5d2581c756f8799b0ecd71002e8c86ae7dc5
Taylor MacDonald <tcmacdonald@gmail.com>
2018-06-18 09:11:56 -0400

    US13869 Implement Pages Add layout to pages migration

--- migrations/20180614135537_create_pages.rb
+++ migrations/20180614135537_create_pages.rb
@@ -15,9 +15,14 @@ class CreatePages < ContentfulMigrations::Migration
         id: @type,
         description: 'A piece of content distinguished by a unique path'
       )
-      content_type.fields.create(id: 'title', name: 'title', type: 'Symbol', required: true)
-      content_type.fields.create(id: 'slug', name: 'slug', type: 'Symbol', required: true, validations: [uniqueness_of])
-      content_type.fields.create(id: 'body', name: 'body', type: 'Text')
+      content_type.fields.create(id: 'title', name: 'Title', type: 'Symbol', required: true)
+      content_type.fields.create(id: 'slug', name: 'Slug', type: 'Symbol', required: true, validations: [uniqueness_of])
+      content_type.fields.create(id: 'body', name: 'Body', type: 'Text')
+
+      validation_in = Contentful::Management::Validation.new
+      validation_in.in = ['default']
+      content_type.fields.create(id: 'layout', name: 'Layout', type: 'Symbol', required: true, validations: [validation_in])
+
       content_type.save
       content_type.publish
       apply_editor(space, 'slug', 'slugEditor')
