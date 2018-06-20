b2d339df3f1b54b18525a5f18bf2f680bc120bff
LeeAnne Galioto <leeanne@helloample.com>
2018-06-20 10:24:31 -0400

    US13752 Series detail landing page down method

--- migrations/20180615112300_update_messages.rb
+++ migrations/20180615112300_update_messages.rb
@@ -1,21 +1,28 @@
 require_relative '../lib/migration_utils'
+require 'pry'
 
-class CreateMessages < ContentfulMigrations::Migration
+class UpdateMessages < ContentfulMigrations::Migration
   include MigrationUtils
-
-  def initialize(name = self.class.name, version = nil, client = nil, space = nil)
-    @type = 'message'
-    super(name, version, client, space)
-  end
 
   def up
     with_space do |space|
+
+      content_type = space.content_types.find('message')
 
       content_type.fields.create(id: 'series', name: 'Series', type: 'Link', link_type: 'Entry', required: true, validations: [validation_of_type('series')])
 
       content_type.save
       content_type.publish
-      apply_editor(space, 'slug', 'slugEditor')
+    end
+  end
+
+  def down
+    with_space do |space|
+      content_type = space.content_types.find('message')
+      binding.pry
+      content_type.fields.destroy('series')
+      content_type.save
+      content_type.publish
     end
   end
 
