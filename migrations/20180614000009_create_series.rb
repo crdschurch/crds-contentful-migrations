f3a0fbb7e130069a275505ecfeb605eb7cba0034
Sean C Davis <scdavis41@gmail.com>
2018-07-02 06:05:46 -0400

    US13318 - Series Content Migration Don't require image on series

--- migrations/20180614000009_create_series.rb
+++ migrations/20180614000009_create_series.rb
@@ -12,7 +12,7 @@ class CreateSeries < RevertableMigration
 
       content_type.fields.create(id: 'title', name: 'Title', type: 'Symbol', required: true)
       content_type.fields.create(id: 'slug', name: 'Slug', type: 'Symbol', required: true, validations: [uniqueness_of])
-      content_type.fields.create(id: 'image', name: 'Image', type: 'Link', link_type: 'Asset', required: true)
+      content_type.fields.create(id: 'image', name: 'Image', type: 'Link', link_type: 'Asset')
       content_type.fields.create(id: 'background_image', name: 'Background Image', type: 'Link', link_type: 'Asset')
       content_type.fields.create(id: 'description', name: 'Description', type: 'Text')
       content_type.fields.create(id: 'starts_at', name: 'Starts', type: 'Date')
