6a081dc83ece3e77bc5590b0e0b4984cff79282a
Luke Moderwell <luke.moderwell@gmail.com>
2018-07-18 15:48:31 -0400

    add featured info to songs model

---
+++ migrations/20180718154330_add_featured_to_songs.rb
@@ -0,0 +1,30 @@
+class AddSoundcloudUrlToSongs < ContentfulMigrations::Migration
+  include MigrationUtils
+
+  def up
+    with_space do |space|
+      content_type = space.content_types.find('song')
+      content_type.fields.create(id: 'call_to_action', name: 'Call To Action', type: 'Symbol')
+      content_type.fields.create(id: 'featured_text', name: 'Featured Text', type: 'Symbol')
+      content_type.fields.create(id: 'featured_label', name: 'Featured Label', type: 'Symbol')
+      content_type.save
+      content_type.publish
+    end
+  end
+
+  def down
+    with_space do |space|
+      content_type = space.content_types.find('song')
+
+      field = content_type.fields.detect { |f| f.id == 'call_to_action' || 'featured_text' || 'featured_label' }
+      field.omitted = true
+      field.disabled = true
+
+      content_type.save
+      content_type.activate
+      content_type.fields.destroy('call_to_action')
+      content_type.fields.destroy('featured_text')
+      content_type.fields.destroy('featured_label')
+    end
+  end
+end
