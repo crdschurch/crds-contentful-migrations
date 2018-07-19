716e988cfca69dc165be9403dae2d93a4b024810
zarahzachz <sarah.sachs@gmail.com>
2018-07-19 13:47:38 -0400

    Make tag titles unique.

--- migrations/20180706000000_create_tags.rb
+++ migrations/20180706000000_create_tags.rb
@@ -10,7 +10,7 @@ class CreateTags < RevertableMigration
         description: 'Content is organized by tags'
       )
 
-      content_type.fields.create(id: 'title', name: 'Title', type: 'Symbol', required: true)
+      content_type.fields.create(id: 'title', name: 'Title', type: 'Symbol', required: true, validations: [uniqueness_of])
       content_type.fields.create(id: 'slug', name: 'Slug', type: 'Symbol', required: true, validations: [uniqueness_of])
 
       content_type.save
@@ -26,5 +26,4 @@ class CreateTags < RevertableMigration
       content_type.destroy
     end
   end
-
 end
