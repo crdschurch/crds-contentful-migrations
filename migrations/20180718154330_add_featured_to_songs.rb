3fa7c3f0620cea89ca1b66d06de45ad4f29a5703
Luke Moderwell <luke.moderwell@gmail.com>
2018-07-20 13:52:53 -0400

    text => subtitle

--- migrations/20180718154330_add_featured_to_songs.rb
+++ migrations/20180718154330_add_featured_to_songs.rb
@@ -5,7 +5,7 @@ class AddSoundcloudUrlToSongs < ContentfulMigrations::Migration
     with_space do |space|
       content_type = space.content_types.find('song')
       content_type.fields.create(id: 'call_to_action', name: 'Call To Action', type: 'Symbol')
-      content_type.fields.create(id: 'featured_text', name: 'Featured Text', type: 'Symbol')
+      content_type.fields.create(id: 'featured_subtitle', name: 'Featured Subtitle', type: 'Symbol')
       content_type.fields.create(id: 'featured_label', name: 'Featured Label', type: 'Symbol')
       content_type.save
       content_type.publish
@@ -16,14 +16,14 @@ class AddSoundcloudUrlToSongs < ContentfulMigrations::Migration
     with_space do |space|
       content_type = space.content_types.find('song')
 
-      field = content_type.fields.detect { |f| f.id == 'call_to_action' || 'featured_text' || 'featured_label' }
+      field = content_type.fields.detect { |f| f.id == 'call_to_action' || 'featured_subtitle' || 'featured_label' }
       field.omitted = true
       field.disabled = true
 
       content_type.save
       content_type.activate
       content_type.fields.destroy('call_to_action')
-      content_type.fields.destroy('featured_text')
+      content_type.fields.destroy('featured_subtitle')
       content_type.fields.destroy('featured_label')
     end
   end
