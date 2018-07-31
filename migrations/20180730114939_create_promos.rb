720e06122b1836ad7616ddc5e6a4267cd834a3a6
Luke Moderwell <luke.moderwell@gmail.com>
2018-07-31 10:54:56 -0400

    US14145: Promo Content Model remove cta field

--- migrations/20180730114939_create_promos.rb
+++ migrations/20180730114939_create_promos.rb
@@ -13,7 +13,6 @@ class CreatePromos < RevertableMigration
       content_type.fields.create(id: 'title', name: 'Title', type: 'Symbol', required: true)
       content_type.fields.create(id: 'image', name: 'Image', type: 'Link', link_type: 'Asset')
       content_type.fields.create(id: 'description', name: 'Description', type: 'Text')
-      content_type.fields.create(id: 'cta', name: 'Call To Action', type: 'Symbol')
       content_type.fields.create(id: 'link_url', name: 'Link URL', type: 'Symbol')
 
       content_type.save
