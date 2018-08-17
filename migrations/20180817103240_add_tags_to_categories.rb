97413dfca830f23b625a55917a2380b3d3e794b1
Sean C Davis <scdavis41@gmail.com>
2018-08-17 10:39:18 -0400

    US14578 - Topic Detail Logic Fix issue with class name on categories.description and add tags to categories

--- migrations/20180817103240_add_tags_to_categories.rb
+++ migrations/20180817103240_add_tags_to_categories.rb
@@ -1,9 +1,11 @@
-class AddDescriptionToCategories < ContentfulMigrations::Migration
+class AddTagsToCategories < ContentfulMigrations::Migration
+
+  include MigrationUtils
 
   def up
     with_space do |space|
       content_type = space.content_types.find('category')
-      content_type.fields.create(id: 'description', name: 'Description', type: 'Text')
+      content_type.fields.create(id: 'tags', name: 'Featured Tags', type: 'Array', items: items_of_type('Entry', ['tag']))
       content_type.save
       content_type.publish
     end
@@ -12,11 +14,11 @@ class AddDescriptionToCategories < ContentfulMigrations::Migration
   def down
     with_space do |space|
       content_type = space.content_types.find('category')
-      field = content_type.fields.detect { |f| f.id == 'description' }
+      field = content_type.fields.detect { |f| f.id == 'tags' }
       field.omitted = true
       field.disabled = true
       content_type.save
-      content_type.fields.destroy('description')
+      content_type.fields.destroy('tags')
       content_type.save
       content_type.publish
     end
