5bfaa73b24ca1da36c2c49109a70e1b7b5f4c9c2
LeeAnne Galioto <leeanne@helloample.com>
2018-08-17 09:54:46 -0400

    US14578 - Topic detail page logic Add Description to Categories, Fix image to category file

--- migrations/20180817095323_add_description_to_categories.rb
+++ migrations/20180817095323_add_description_to_categories.rb
@@ -3,7 +3,7 @@ class AddImageToCategories < ContentfulMigrations::Migration
   def up
     with_space do |space|
       content_type = space.content_types.find('category')
-      content_type.fields.create(id: 'image', name: 'Image', type: 'Link', link_type: 'Asset')
+      content_type.fields.create(id: 'description', name: 'Description', type: 'Text')
       content_type.save
       content_type.publish
     end
@@ -12,11 +12,11 @@ class AddImageToCategories < ContentfulMigrations::Migration
   def down
     with_space do |space|
       content_type = space.content_types.find('category')
-      field = content_type.fields.detect { |f| f.id == 'image' }
+      field = content_type.fields.detect { |f| f.id == 'description' }
       field.omitted = true
       field.disabled = true
       content_type.save
-      content_type.fields.destroy('image')
+      content_type.fields.destroy('description')
       content_type.save
       content_type.publish
     end
