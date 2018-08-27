713dd77ab0a6c1b1cdea041c3ccaa43662efe3ac
zarahzachz <sarah.sachs@gmail.com>
2018-08-27 14:29:08 -0400

    Add help text programmatically to chaser question title.

--- migrations/20180827101611_create_chaser_questions.rb
+++ migrations/20180827101611_create_chaser_questions.rb
@@ -15,6 +15,17 @@ class CreateChaserQuestions < RevertableMigration
 
       content_type.save
       content_type.publish
+
+      with_editor_interfaces do |editor_interfaces|
+        editor_interface = editor_interfaces.default(space.id, content_type_id)
+        controls = editor_interface.controls
+        controls.detect { |e| e['fieldId'] == 'title' }['settings'] = { 'helpText' => 'If your question is associated with a Chaser, a descriptive title will make it easier to find and link it appropriately. Note: This field is for labeling purposes and will not be reflected outside of Contentful.' }
+        editor_interface.update(controls: controls)
+        editor_interface.reload
+      end
+
+      content_type.save
+      content_type.publish
     end
   end
 end
