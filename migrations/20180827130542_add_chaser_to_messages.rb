52395f1edc9b5277a15dd1cddc51bbb7209c6c84
zarahzachz <sarah.sachs@gmail.com>
2018-08-27 13:08:19 -0400

    Add ability to add Chaser object to articles and messages.

---
+++ migrations/20180827130542_add_chaser_to_messages.rb
@@ -0,0 +1,26 @@
+class AddChaserToMessages < ContentfulMigrations::Migration
+  include MigrationUtils
+
+  def up
+    with_space do |space|
+      content_type = space.content_types.find('message')
+      content_type.fields.create(id: 'chaser', name: 'The Chaser', type: 'Link', link_type: 'Entry', validations: [validation_of_type('chaser')])
+      content_type.save
+      content_type.publish
+    end
+  end
+
+  def down
+    with_space do |space|
+      content_type = space.content_types.find('message')
+
+      field = content_type.fields.detect { |f| f.id == 'chaser' }
+      field.omitted = true
+      field.disabled = true
+
+      content_type.save
+      content_type.activate
+      content_type.fields.destroy('chaser')
+    end
+  end
+end
