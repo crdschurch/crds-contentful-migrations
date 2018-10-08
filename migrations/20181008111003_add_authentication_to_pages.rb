71632454f3025d9ccfa2156e4ef6832270db1866
Luke Moderwell <luke.moderwell@gmail.com>
2018-10-08 11:52:09 -0400

    US15255: Add Auth to Pages remove help text

--- migrations/20181008111003_add_authentication_to_pages.rb
+++ migrations/20181008111003_add_authentication_to_pages.rb
@@ -8,15 +8,6 @@ class AddAuthenticationToPages < ContentfulMigrations::Migration
       
       content_type.fields.create(id: 'requires_auth', name: 'Requires Authentication?', type: 'Boolean')
 
-      # Set Editor UI
-      with_editor_interfaces do |editor_interfaces|
-        editor_interface = editor_interfaces.default(space_id, 'page')
-        controls = editor_interface.controls
-        controls.detect { |e| e['fieldId'] == 'requires_auth' }['settings'] = { 'helpText' => 'Does the user need to be logged in to see this page?' }
-        editor_interface.update(controls: controls)
-        editor_interface.reload
-      end
-
       content_type.save
       content_type.publish
     end
