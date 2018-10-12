ea8f985478cbe1451f9a0afc2b8a42434e6252b4
Brian Miller <bzmiller82@gmail.com>
2018-10-12 11:31:54 -0400

    Added Perspectives and removed Commentaries

--- migrations/20181012095507_create_perspectives.rb
+++ migrations/20181012095507_create_perspectives.rb
@@ -1,11 +1,11 @@
-class CreateCommentaries < RevertableMigration
+class CreatePerspectives < RevertableMigration
 
-  self.content_type_id = 'commentary'
+  self.content_type_id = 'perspective'
 
   def up
     with_space do |space|
       content_type = space.content_types.create(
-        name: 'Commentary',
+        name: 'Perspective',
         id: content_type_id,
         description: 'Short paragraph reactions to current worldly news events with a different perspective.'
       )
@@ -16,7 +16,6 @@ class CreateCommentaries < RevertableMigration
       content_type.fields.create(id: 'image', name: 'Image', type: 'Link', link_type: 'Asset')
       content_type.fields.create(id: 'published_at', name: 'Published At', type: 'Date', required: true)
       content_type.fields.create(id: 'author', name: 'Author', type: 'Link', link_type: 'Entry', required: true, validations: [validation_of_type('author')])
-      content_type.fields.create(id: 'category', name: 'Category', type: 'Link', link_type: 'Entry', required: true, validations: [validation_of_type('category')])
       content_type.fields.create(id: 'tags', name: 'Tags', type: 'Array', items: items_of_type('Entry', 'tag'))
       content_type.fields.create(id: 'discussion', name: 'Discussion', type: 'Link', link_type: 'Entry', validations: [validation_of_type('discussion')])
 
