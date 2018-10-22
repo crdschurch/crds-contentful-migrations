f6fadb8bcb4109032619a9cd3963aafa573dd1b6
Sean C Davis <scdavis41@gmail.com>
2018-10-22 12:04:45 -0400

    US15347 - Meta Content Model Add image to perspective set

--- migrations/20181019152419_create_perspective_set.rb
+++ migrations/20181019152419_create_perspective_set.rb
@@ -12,6 +12,7 @@ class CreatePerspectiveSet < RevertableMigration
 
       content_type.fields.create(id: 'title', name: 'Title', type: 'Symbol', required: true)
       content_type.fields.create(id: 'slug', name: 'Slug', type: 'Symbol', required: true, validations: [uniqueness_of])
+      content_type.fields.create(id: 'image', name: 'Image', type: 'Link', link_type: 'Asset')
       content_type.fields.create(id: 'perspectives', name: 'Perspectives', type: 'Array', items: items_of_type('Entry', ['perspective']))
       content_type.fields.create(id: 'published_at', name: 'Published At', type: 'Date', required: true)
       content_type.fields.create(id: 'meta', name: 'Meta', type: 'Link', link_type: 'Entry', validations: [validation_of_type('meta')])
