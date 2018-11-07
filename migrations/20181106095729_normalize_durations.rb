664b08aad3796a0ceef6707493c7434b9504a758
Taylor C. MacDonald <tcmacdonald@gmail.com>
2018-11-07 10:33:01 -0500

    US15483 Durations Implement durations field

---
+++ migrations/20181106095729_normalize_durations.rb
@@ -0,0 +1,28 @@
+class NormalizeDurations < ContentfulMigrations::Migration
+
+  include MigrationUtils
+  TYPES = %w(article video song episode message)
+
+  def up
+    with_space do |space|
+      TYPES.each do |type|
+        content_type = space.content_types.find(type)
+        content_type.fields.create(id: 'duration', name: 'Duration (seconds)', type: 'Number', disabled: true)
+        content_type.save
+        content_type.publish
+      end
+    end
+  end
+
+  def down
+    with_space do |space|
+      TYPES.each do |type|
+        content_type = space.content_types.find(type)
+        content_type.fields.destroy('duration')
+        content_type.save
+        content_type.publish
+      end
+    end
+  end
+
+end
