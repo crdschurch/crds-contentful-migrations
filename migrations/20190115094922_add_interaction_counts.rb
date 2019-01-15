2659fa6c753950221d19c125f8432ee96c0945f3
Taylor C. MacDonald <tcmacdonald@gmail.com>
2019-01-15 10:01:21 -0500

    US16102 Interactions Add interaction count fields to various content-types

--- migrations/20190115094922_add_interaction_counts.rb
+++ migrations/20190115094922_add_interaction_counts.rb
@@ -1,27 +1,33 @@
 class AddInteractionCounts < ContentfulMigrations::Migration
   include MigrationUtils
 
+  content_types = %w(album article author episode message podcast series song video)
+
   def up
     with_space do |space|
-      content_type = space.content_types.find('article')
-      content_type.fields.create(id: 'interaction_count', name: 'Interaction Count', type: 'Number', disabled: true)
-      content_type.save
-      content_type.publish
+      content_types.each do |id|
+        content_type = space.content_types.find(id)
+        content_type.fields.create(id: 'interaction_count', name: 'Interaction Count', type: 'Number', disabled: true)
+        content_type.save
+        content_type.publish
+      end
     end
   end
 
   def down
     with_space do |space|
-      content_type = space.content_types.find('article')
-      field = content_type.fields.detect { |f| f.id == 'interaction_count' }
-      field.omitted = true
-      field.disabled = true
+      content_types.each do |id|
+        content_type = space.content_types.find(id)
+        field = content_type.fields.detect { |f| f.id == 'interaction_count' }
+        field.omitted = true
+        field.disabled = true
 
-      content_type.save
-      content_type.activate
-      content_type.fields.destroy('interaction_count')
-      content_type.save
-      content_type.publish
+        content_type.save
+        content_type.activate
+        content_type.fields.destroy('interaction_count')
+        content_type.save
+        content_type.publish
+      end
     end
   end
 end
