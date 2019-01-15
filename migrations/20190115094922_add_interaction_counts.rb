a4a1a7869808deb0e26ffba241aff382aea8bc0e
Taylor C. MacDonald <tcmacdonald@gmail.com>
2019-01-15 09:57:06 -0500

    US16102 Interactions Add interaction count field to article content model

---
+++ migrations/20190115094922_add_interaction_counts.rb
@@ -0,0 +1,27 @@
+class AddInteractionCounts < ContentfulMigrations::Migration
+  include MigrationUtils
+
+  def up
+    with_space do |space|
+      content_type = space.content_types.find('article')
+      content_type.fields.create(id: 'interaction_count', name: 'Interaction Count', type: 'Number', disabled: true)
+      content_type.save
+      content_type.publish
+    end
+  end
+
+  def down
+    with_space do |space|
+      content_type = space.content_types.find('article')
+      field = content_type.fields.detect { |f| f.id == 'interaction_count' }
+      field.omitted = true
+      field.disabled = true
+
+      content_type.save
+      content_type.activate
+      content_type.fields.destroy('interaction_count')
+      content_type.save
+      content_type.publish
+    end
+  end
+end
