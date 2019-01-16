aad3783d5f15b879339a93f810b3872339b723ed
Sean C Davis <scdavis41@gmail.com>
2019-01-16 13:32:58 -0500

    US16125 - Contentful Seed Data Simplify working with links and arrays

--- data/test-1.md
+++ data/test-1.md
@@ -2,12 +2,25 @@
 _content_type: article
 # _body_field: description
 title: Article Title
-image: asset:4Zxm0bwR9eWSiuw442WAqS
-author: entry:2VBUQyLmh22eacKWw08E4o
+image: 4Zxm0bwR9eWSiuw442WAqS
+author: 2VBUQyLmh22eacKWw08E4o
 tags:
-    - entry:luZPICyHVQUYQYSAa6Imu
-    - entry:1JWjoImLpWAeWMCWI2WOaM
-    - entry:31xMlxGaBWKw0aYK2ESgQA
+  - luZPICyHVQUYQYSAa6Imu
+  - 1JWjoImLpWAeWMCWI2WOaM
+  - 31xMlxGaBWKw0aYK2ESgQA
+images_test:
+  - 4Zxm0bwR9eWSiuw442WAqS
+  - 3pCpGHrt9YS6aUsY2My0ug
+list_test:
+  - THING_1
+  - THING_2
+
+# image: asset:4Zxm0bwR9eWSiuw442WAqS
+# author: entry:2VBUQyLmh22eacKWw08E4o
+# tags:
+#     - entry:luZPICyHVQUYQYSAa6Imu
+#     - entry:1JWjoImLpWAeWMCWI2WOaM
+#     - entry:31xMlxGaBWKw0aYK2ESgQA
 ---
 
 This is the body
--- lib/seeder.rb
+++ lib/seeder.rb
@@ -14,27 +14,6 @@ class Seeder
       environment = contentful.environments(ENV['CONTENTFUL_SPACE_ID']).find(ENV['CONTENTFUL_ENV'] || 'master')
       content_types = environment.content_types.all.map { |t| [t.id, t] }.to_h
 
-      puts 'Fetching assets ...'
-      asset_request = environment.assets.all(limit: 1000)
-      self.assets = asset_request.to_a
-      (asset_request.total / 1000).times do
-        puts 'Fetching next page ...'
-        asset_request = asset_request.next_page
-        assets.concat(asset_request.to_a)
-      end
-      assets.select! { |a| a.file }.compact
-
-      puts 'Fetching entries ...'
-      entry_request = environment.entries.all(limit: 1000)
-      self.entries = entry_request.to_a
-      (entry_request.total / 1000).times do
-        puts 'Fetching next page ...'
-        entry_request = entry_request.next_page
-        entries.concat(entry_request.to_a)
-      end
-
-      # binding.pry
-
       files.each do |file|
         body = File.read(file)
         raw_frontmatter = body.match(/^---(.|\n)*?---/)[0]
@@ -42,19 +21,21 @@ class Seeder
         body.slice!(raw_frontmatter)
         body.strip!
 
-        content_type = frontmatter['_content_type']
+        content_type = content_types[frontmatter['_content_type']]
         linked_fields = frontmatter['_linked_fields']
         frontmatter[frontmatter['_body_field'] || 'body'] = body if body
         frontmatter.keys.select { |k| k.start_with?('_') }.map { |k| frontmatter.delete(k) }
         frontmatter.symbolize_keys!
 
-        frontmatter.each do |k, v|
-          next unless [v].flatten.first.match(/^(asset|entry):/)
-          attachments = [v].flatten.map { |a| send(a.split(':').first.pluralize).detect { |x| x.id == a.split(':').last } }
+        frontmatter.each do |k,v|
+          field = content_type.fields.detect { |f| f.id == k.to_s }
+          next unless field && %w(Link Array).include?(field.type)
+          next unless link_type = field.link_type || field.items.link_type
+          attachments = [v].flatten.map { |id| environment.send(link_type.downcase.pluralize).find(id) }.compact
           frontmatter[k] = v.is_a?(Array) ? attachments : attachments.first if attachments
         end
 
-        entry = content_types[content_type].entries.create(frontmatter)
+        entry = content_type.entries.create(frontmatter)
         entry.publish
       end
     end
