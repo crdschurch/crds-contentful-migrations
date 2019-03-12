35e7914329b446fbd17d1de74691dc855755a989
Jimmy Janssen <jjanssen@CSMBP0010-JJ.local>
2019-03-12 10:08:35 -0400

    feature/US16667-add-unpublished-at

---
+++ migrations/20190311175201_add_unpublished_at_to_song.rb
@@ -0,0 +1,13 @@
+class AddUnpublishedAtToSong < ContentfulMigrations::Migration
+
+    include MigrationUtils
+    def up
+      with_space do |space|
+        content_type = space.content_types.find('song')
+        content_type.fields.create(id: 'unpublished_at', name: 'Unpublished At', type: 'Date')
+        content_type.save
+        content_type.publish
+      end
+    end
+  end
+  
