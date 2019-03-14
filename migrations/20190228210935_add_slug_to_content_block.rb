0268a91ef1a5ae6b0b00b3921fe0df815e70d741
Jeff Lay <phritolay@icloud.com>
2019-03-14 16:39:43 -0400

    de6522 content title should be for Contentful only
    update slug to be automatically generated from title field

--- migrations/20190228210935_add_slug_to_content_block.rb
+++ migrations/20190228210935_add_slug_to_content_block.rb
@@ -1,8 +1,23 @@
 class AddSlugToContentBlock < ContentfulMigrations::Migration
+
+  include MigrationUtils
+
   def up
     with_space do |space|
       content_type = space.content_types.find('content_block')
       content_type.fields.create(id: 'slug', name: 'Slug', type: 'Symbol', required: true)
+      content_type.save
+      content_type.publish
+
+      editor_interface = content_type.editor_interface.default
+      controls = editor_interface.controls
+      controls.detect { |c| c['fieldId'] == 'slug' }['widgetId'] = 'slugEditor'
+      editor_interface.update(controls: controls)
+      editor_interface.reload
+
+      field = content_type.fields.detect { |f| f.id == 'slug' }
+      field.disabled = true
+
       content_type.save
       content_type.publish
     end
