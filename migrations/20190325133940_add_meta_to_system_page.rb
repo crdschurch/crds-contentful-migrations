69fd09046f681cfceca87d8ce7c37a454ef14095
j-brammer <jbrammer@callibrity.com>
2019-03-26 08:59:48 -0400

    migration to add meta to system page

---
+++ migrations/20190325133940_add_meta_to_system_page.rb
@@ -0,0 +1,27 @@
+class AddMetaToSystemPage < ContentfulMigrations::Migration
+
+  include MigrationUtils
+  
+  def up
+    with_space do |space|
+      content_type = space.content_types.find('system_page')
+      content_type.fields.create(id: 'meta', name: 'Meta', type: 'Link', link_type: 'Entry', validations: [validation_of_type('meta')])
+      content_type.save
+      content_type.publish
+    end
+  end
+
+  def down
+    with_space do |space|
+      content_type = space.content_types.find('system_page')
+      
+      field = content_type.fields.detect { |f| f.id == 'meta' }
+      field.omitted = true
+      field.disabled = true
+
+      content_type.save
+      content_type.activate
+      content_type.fields.destroy('meta')
+    end
+  end
+end
