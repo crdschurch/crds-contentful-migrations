cde9e152a2000f22cca1eb335d74f47f2c461d14
Sean C Davis <scdavis41@gmail.com>
2019-03-27 09:59:32 -0400

    US14397 - Timestamps Widget IDs are different per environment

--- migrations/20190326151737_add_timestamps_to_messages_and_videos.rb
+++ migrations/20190326151737_add_timestamps_to_messages_and_videos.rb
@@ -7,10 +7,17 @@ class AddTimestampsToMessagesAndVideos < ContentfulMigrations::Migration
         content_type.save
         content_type.publish
 
+        widget_id = {
+          'dev-test' => '6CUhmTcawrUtK8ysMBww7z',
+          'int' => '16gOjwpCH2RxTryekmyCQR',
+          'demo' => '4WnO5Wf1O6Aa2dhxQO5vjj',
+          'master' => '7ArP5EZMYZWpGH08FIy0IA'
+        }[ENV['CONTENTFUL_ENV'] || 'master']
+
         editor_interface = content_type.editor_interface.default
         controls = editor_interface.controls
         controls.detect { |c| c['fieldId'] == 'timestamps' }['widgetNamespace'] = 'extension'
-        controls.detect { |c| c['fieldId'] == 'timestamps' }['widgetId'] = '7ArP5EZMYZWpGH08FIy0IA'
+        controls.detect { |c| c['fieldId'] == 'timestamps' }['widgetId'] = widget_id
         editor_interface.update(controls: controls)
         editor_interface.reload
 
@@ -22,13 +29,15 @@ class AddTimestampsToMessagesAndVideos < ContentfulMigrations::Migration
 
   def down
     with_space do |space|
-      content_type = space.content_types.find('collection')
-      field = content_type.fields.detect { |f| f.id == 'timestamps' }
-      field.omitted = true
-      field.disabled = true
-      content_type.save
-      content_type.activate
-      content_type.fields.destroy('timestamps')
+      %w(message video).each do |type|
+        content_type = space.content_types.find(type)
+        field = content_type.fields.detect { |f| f.id == 'timestamps' }
+        field.omitted = true
+        field.disabled = true
+        content_type.save
+        content_type.activate
+        content_type.fields.destroy('timestamps')
+      end
     end
   end
 end
