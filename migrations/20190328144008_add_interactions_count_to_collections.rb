5d5d9bfacd155bfc055167bafbb8489d7944f74b
Taylor C. MacDonald <tcmacdonald@gmail.com>
2019-03-28 14:46:17 -0400

    DE6611 Interactions Implement interactions_count field on collection content-type

---
+++ migrations/20190328144008_add_interactions_count_to_collections.rb
@@ -0,0 +1,24 @@
+class AddInteractionsCountToCollections < ContentfulMigrations::Migration
+  def up
+    with_space do |space|
+      content_type = space.content_types.find('collection')
+      content_type.fields.create(id: 'interaction_count', name: 'Interaction Count', type: 'Number', disabled: true)
+      content_type.save
+      content_type.publish
+    end
+  end
+
+  def down
+    with_space do |space|
+      content_type = space.content_types.find('collection')
+      field = content_type.fields.detect { |f| f.id == 'interaction_count' }
+      field.omitted = true
+      field.disabled = true
+      content_type.save
+      content_type.activate
+      content_type.fields.destroy('interaction_count')
+      content_type.save
+      content_type.publish
+    end
+  end
+end
