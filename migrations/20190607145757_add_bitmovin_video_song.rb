df1a4d0d87c0beecce718ebd328e3f165de812f1
j-brammer <jbrammer@callibrity.com>
2019-06-11 14:37:46 -0400

    added transcription

--- migrations/20190607145757_add_bitmovin_video_song.rb
+++ migrations/20190607145757_add_bitmovin_video_song.rb
@@ -10,6 +10,7 @@ class AddBitmovinVideoSong < ContentfulMigrations::Migration
 
         content_type.fields.create(id: 'video_file', name: 'Video File', type: 'Link', link_type: 'Asset', validations: [require_mime_type(:video)])
         content_type.fields.create(id: 'bitmovin_url', name: 'Bitmovin Url', type: 'Symbol')
+        content_type.fields.create(id: 'transcription', name: 'Transcription', type: 'Link', link_type: 'Asset')
 
         content_type.save
         content_type.publish
@@ -33,10 +34,15 @@ class AddBitmovinVideoSong < ContentfulMigrations::Migration
         bitmovin_url.omitted = true
         bitmovin_url.disabled = true
 
+        transcription = content_type.fields.detect { |f| f.id == 'transcription' }
+        transcription.omitted = true
+        transcription.disabled = true
+
         content_type.save
         content_type.activate
         content_type.fields.destroy('video_file')
         content_type.fields.destroy('bitmovin_url')
+        content_type.fields.destroy('transcription')
       end
 
     end
