673cccd8adacda6f34f3131bfb64705a78cf7f36
Josh Byron <joshhbyron@gmail.com>
2019-07-12 15:47:26 -0400

    US17684-music-migrations 	- Updates song migration to accomodate for crdmusic changes

--- migrations/20180614000006_create_songs.rb
+++ migrations/20180614000006_create_songs.rb
@@ -10,36 +10,34 @@ class CreateSongs < RevertableMigration
         description: 'An individual song'
       )
 
+      # 
+      content_type.fields.create(id: 'ccli_number', name: 'CCLI Number', type: 'Symbol', required: true)
+      content_type.fields.create(id: 'written_by', name: 'Written By', type: 'Symbol', required: true)
+      content_type.fields.create(id: 'song_select_url', name: 'Song Select Url', type: 'Link', required: true)
+      
+      content_type.fields.create(id: 'video', name: 'Video', type: 'Link', link_type: 'Asset', validations: [require_mime_type(:video)])
+      content_type.fields.create(id: 'lyric_file', name: 'Lyric File', type: 'Link', link_type: 'Asset')
+      content_type.fields.create(id: 'chords_file', name: 'Chords File', type: 'Link', link_type: 'Asset')
+
       content_type.fields.create(id: 'title', name: 'Title', type: 'Symbol', required: true)
       content_type.fields.create(id: 'slug', name: 'Slug', type: 'Symbol', required: true, validations: [uniqueness_of])
-      content_type.fields.create(id: 'description', name: 'Description', type: 'Text')
-      content_type.fields.create(id: 'details', name: 'Details', type: 'Text')
       content_type.fields.create(id: 'lyrics', name: 'Lyrics', type: 'Text')
-      content_type.fields.create(id: 'chords', name: 'Chords', type: 'Text')
       content_type.fields.create(id: 'stems', name: 'Stems', type: 'Text')
 
       items = Contentful::Management::Field.new
       items.type = 'Symbol'
       content_type.fields.create(id: 'tags', name: 'Tags', type: 'Array', items: items)
 
-      content_type.fields.create(id: 'category', name: 'Category', type: 'Link', link_type: 'Entry', validations: [validation_of_type('category')])
-      content_type.fields.create(id: 'author', name: 'Author', type: 'Array', items: items_of_type('Entry', 'author'))
-
-      content_type.fields.create(id: 'audio_duration', name: 'Audio Duration', type: 'Text')
-      content_type.fields.create(id: 'album', name: 'Album', type: 'Link', link_type: 'Entry', validations: [validation_of_type('author')])
-      content_type.fields.create(id: 'image', name: 'Image', type: 'Link', link_type: 'Asset', required: true)
       content_type.fields.create(id: 'bg_image', name: 'Background Image', type: 'Link', link_type: 'Asset')
       content_type.fields.create(id: 'spotify_url', name: 'Spotify URL', type: 'Symbol')
       content_type.fields.create(id: 'apple_music_url', name: 'Apple Music URL', type: 'Symbol')
       content_type.fields.create(id: 'google_play_url', name: 'Google Play URL', type: 'Symbol')
       content_type.fields.create(id: 'youtube_url', name: 'YouTube URL', type: 'Symbol')
       content_type.fields.create(id: 'published_at', name: 'Published At', type: 'Date', required: true)
-      content_type.fields.create(id: 'related_videos', name: 'Related Videos', type: 'Array', items: items_of_type('Entry', 'video'))
 
       content_type.save
       content_type.publish
       apply_editor(space, 'slug', 'slugEditor')
     end
   end
-
 end
