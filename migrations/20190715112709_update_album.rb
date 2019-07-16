2a848a385c930757cf0e9c772702799a2890c43f
Josh Byron <joshhbyron@gmail.com>
2019-07-16 10:40:13 -0400

    US17684-music-migrations 	- Updates up method for album to correctly destroy/create fields 	- Adds down method

---
+++ migrations/20190715112709_update_album.rb
@@ -0,0 +1,70 @@
+class UpdateAlbum < ContentfulMigrations::Migration
+
+  include MigrationUtils
+
+  def up
+    with_space do |space|
+
+      content_type = space.content_types.find('album')
+
+      # Delete Fields
+      fields = ['description', 'author']
+      fields.each do |field|
+
+        field = content_type.fields.detect { |f| f.id == field }
+        next unless
+        field.omitted = true
+        field.disabled = true
+
+      end
+
+      content_type.save
+      content_type.publish
+
+      fields.each do |field|
+        content_type.fields.destroy(field)
+      end
+
+      # Create Fields
+      content_type.fields.create(id: 'behind_the_scenes', name: 'Behind the scenes', type: 'Link', link_type: 'Asset', validations: [require_mime_type(:video)])
+      content_type.fields.create(id: 'songs', name: 'Songs', type: 'Array', items: items_of_type('Entry', 'song'))
+
+      content_type.save
+      content_type.publish
+
+    end
+  end
+
+  def down 
+
+    with_space do |space|
+
+      content_type = space.content_types.find('album')
+
+      # Delete Fields
+      fields = ['behind_the_scenes', 'songs']
+      fields.each do |field|
+
+        field = content_type.fields.detect { |f| f.id == field }
+        next unless
+        field.omitted = true
+        field.disabled = true
+
+      end
+
+      content_type.save
+      content_type.publish
+
+      fields.each do |field|
+        content_type.fields.destroy(field)
+      end
+
+      # Create fields
+      content_type.fields.create(id: 'description', name: 'Description', type: 'Text')
+      content_type.fields.create(id: 'author', name: 'Author', type: 'Link', link_type: 'Entry', required: true, validations: [validation_of_type('author')])
+
+      content_type.save
+      content_type.publish
+    end 
+  end
+end
