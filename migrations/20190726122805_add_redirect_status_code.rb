ffeab25a8b5ecf3c8a45d770f423081c82b2d793
Josh Byron <joshhbyron@gmail.com>
2019-07-26 15:47:32 -0400

    US17546-redirect-unpublished-pages - Adds is_forced field

---
+++ migrations/20190726122805_add_redirect_status_code.rb
@@ -0,0 +1,65 @@
+class AddRedirectStatusCode < ContentfulMigrations::Migration
+
+  include MigrationUtils
+
+  def up
+    with_space do |space|
+
+      ## Create Content model
+      content_type = space.content_types.find('redirect')
+
+      ## Create validation
+      validation_for_type = Contentful::Management::Validation.new
+      validation_for_type.in = [200,301,302,401,410]
+      content_type.fields.create(id: 'status_code', name: 'Status code', type: 'Integer', validations: [validation_for_type])
+
+      content_type.fields.create(id: 'is_forced', name: 'Is forced', type: 'Boolean')
+      
+      ## Publish
+      content_type.save
+      content_type.publish
+
+      ## Editor interface configuration
+      editor_interface = content_type.editor_interface.default
+      controls = editor_interface.controls
+      field = controls.detect { |e| e['fieldId'] == 'snail_trail' }
+      field['widgetId'] = "dropdown"
+      field['settings'] = { 'helpText' => 'If blank, validation will default to 301' }
+      editor_interface.update(controls: controls)
+      editor_interface.reload
+
+      content_type.save
+      content_type.publish
+
+    end
+  end
+
+  def down 
+
+    with_space do |space|
+
+      content_type = space.content_types.find('redirect')
+
+      # Delete Fields
+      fields = ['status_code']
+      fields.each do |field|
+
+        field = content_type.fields.detect { |f| f.id == field }
+        next unless
+        field.omitted = true
+        field.disabled = true
+
+      end
+
+      content_type.save
+      content_type.publish
+
+      fields.each do |field|
+        content_type.fields.destroy(field)
+      end
+
+      content_type.save
+      content_type.publish
+    end 
+  end
+end
