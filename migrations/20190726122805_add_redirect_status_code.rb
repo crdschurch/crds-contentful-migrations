e771e6761cf275a638adceaae44a0bff25c87342
Josh Byron <joshhbyron@gmail.com>
2019-07-31 16:29:36 -0400

    feature/US17546-redirect-unpublished-pages - Adds helper text to is_forced field

--- migrations/20190726122805_add_redirect_status_code.rb
+++ migrations/20190726122805_add_redirect_status_code.rb
@@ -26,6 +26,10 @@ class AddRedirectStatusCode < ContentfulMigrations::Migration
       field = controls.detect { |e| e['fieldId'] == 'status_code' }
       field['widgetId'] = "dropdown"
       field['settings'] = { 'helpText' => 'If blank, validation will default to 302' }
+
+      field = controls.detect { |e| e['fieldId'] == 'is_forced' }
+      field['settings'] = { 'helpText' => 'Check if you’re 100% sure that you’ll always want to redirect, even when the URL matches a static file.' }
+
       editor_interface.update(controls: controls)
       editor_interface.reload
 
