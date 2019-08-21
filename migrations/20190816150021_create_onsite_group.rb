4b73078ff2fd10ee74c9d57c8f4c55cc55f6173c
Josh Byron <joshhbyron@gmail.com>
2019-08-21 11:51:28 -0400

    US17792-create-onsite-group-model - Resolves most requested changes

--- migrations/20190816150021_create_onsite_group.rb
+++ migrations/20190816150021_create_onsite_group.rb
@@ -5,16 +5,16 @@ class CreateOnsiteGroup < RevertableMigration
   def up
     with_space do |space|
       content_type = space.content_types.create(	
-         name: 'Onsite group',	
+         name: 'Onsite Group',	
          id: content_type_id,	
          description: 'An onsite group'	
        )
 
-       content_type.fields.create(id:'title', name:'Title', type:'Symbol', required: true)	
-       content_type.fields.create(id:'slug', name:'slug', type:'Symbol', required:true)
-       content_type.fields.create(id:'description', name:'Description', type:'Symbol', required:true)	
-       content_type.fields.create(id:'detail', name:'Detail', type:'Symbol', required:true)
-       content_type.fields.create(id:'meetings', name:'Meetings', type:'Array', items:items_of_type('Entry', 'onsite_group_meeting'))
+       content_type.fields.create(id: 'title', name: 'Title', type: 'Symbol', required: true)	
+       content_type.fields.create(id: 'slug', name: 'slug', type: 'Symbol', required: true)
+       content_type.fields.create(id: 'description', name: 'Description', type: 'Text', required: true)	
+       content_type.fields.create(id: 'detail', name: 'Detail', type: 'Text', required: true)
+       content_type.fields.create(id: 'meetings', name: 'Meetings', type: 'Array', items: items_of_type('Entry', 'onsite_group_meeting'))
        content_type.fields.create(id: 'category', name: 'Category', type: 'Link', link_type: 'Entry', validations: [validation_of_type('onsite_group_category')])
 
        content_type.save	
