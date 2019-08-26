336f83f014b0248a8c1bb650c68bf087b679de2d
Josh Byron <joshhbyron@gmail.com>
2019-08-26 12:12:15 -0400

    US17792-create-onsite-group-model - Adds length field - Adds help text for length and field

--- migrations/20190816150021_create_onsite_group.rb
+++ migrations/20190816150021_create_onsite_group.rb
@@ -10,15 +10,29 @@ class CreateOnsiteGroup < RevertableMigration
          description: 'An onsite group'	
        )
 
-       content_type.fields.create(id: 'title', name: 'Title', type: 'Symbol', required: true)	
-       content_type.fields.create(id: 'slug', name: 'slug', type: 'Symbol', required: true)
-       content_type.fields.create(id: 'description', name: 'Description', type: 'Text', required: true)	
-       content_type.fields.create(id: 'detail', name: 'Detail', type: 'Text', required: true)
-       content_type.fields.create(id: 'meetings', name: 'Meetings', type: 'Array', items: items_of_type('Entry', 'onsite_group_meeting'))
-       content_type.fields.create(id: 'category', name: 'Category', type: 'Link', link_type: 'Entry', validations: [validation_of_type('onsite_group_category')])
+      content_type.fields.create(id: 'title', name: 'Title', type: 'Symbol', required: true)	
+      content_type.fields.create(id: 'slug', name: 'slug', type: 'Symbol', required: true)
+      content_type.fields.create(id: 'description', name: 'Description', type: 'Text', required: true)	
+      content_type.fields.create(id: 'length', name: 'Length', type: 'Symbol', required: true)	
+      content_type.fields.create(id: 'detail', name: 'Detail', type: 'Text', required: true)
+      content_type.fields.create(id: 'meetings', name: 'Meetings', type: 'Array', items: items_of_type('Entry', 'onsite_group_meeting'))
+      content_type.fields.create(id: 'category', name: 'Category', type: 'Link', link_type: 'Entry', validations: [validation_of_type('onsite_group_category')])
 
-       content_type.activate
-       apply_editor(content_type, 'slug', 'slugEditor')	
+      content_type.save
+      content_type.publish
+
+      editor_interface = content_type.editor_interface.default
+      controls = editor_interface.controls
+
+      controls.detect { |e| e['fieldId'] == 'Length' }['settings'] = { 'helpText' => 'How long the group will be active for . ie. 12 weeks' }
+      controls.detect { |e| e['fieldId'] == 'Detail' }['settings'] = { 'helpText' => 'Who the group is perfect for. ie. Woman of all ages' }
+
+      editor_interface.update(controls: controls)
+      editor_interface.reload
+
+      content_type.activate
+      apply_editor(content_type, 'slug', 'slugEditor')	
+      content_type.publish
     end
   end
 end
