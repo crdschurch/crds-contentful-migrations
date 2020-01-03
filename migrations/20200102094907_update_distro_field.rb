f3dbc2de5f03c093fe023c71cbd6e6eb55d3f2ce
Josh Byron <joshhbyron@gmail.com>
2020-01-03 09:48:25 -0500

    - Removes unnecessary variable

--- migrations/20200102094907_update_distro_field.rb
+++ migrations/20200102094907_update_distro_field.rb
@@ -43,17 +43,25 @@ class UpdateDistroField < ContentfulMigrations::Migration
 
   def down
     with_space do |space|
-      content_types = %w[Album Article Category Collection Content Block Episode Message Meta Page Podcast Redirect Series Song Video]
+      content_types = [ 
+        'album',
+        'article',
+        'content_block',
+        'category',
+        'collection',
+        'episode',
+        'message',
+        'meta',
+        'page',
+        'podcast',
+        'redirect',
+        'series',
+        'song',
+        'video'
+      ]
 
       content_types.each do |name|
         content_type = space.content_types.find(name)
-        
-        widget_id = {
-          'development' => '7pen61ICvT0RvmFXRepZa3',
-          'int' => '2XO3c2aXJj0MDelzxDwB3f',
-          'demo' => '1MzJ8eqfVidEFElMG2DIPS',
-          'master' => '4ZENvJxZkqJreQMNHEruk6'
-        }[ENV['CONTENTFUL_ENV'] || 'master']
   
         editor_interface = content_type.editor_interface.default
         controls = editor_interface.controls
