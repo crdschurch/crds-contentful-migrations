a2fe4bb4e5655cfac1ff3585b8a0426671bd24cb
Taylor C. MacDonald <tcmacdonald@gmail.com>
2020-01-06 12:05:08 -0500

    Fix broken build

--- migrations/20200102094907_update_distro_field.rb
+++ migrations/20200102094907_update_distro_field.rb
@@ -2,12 +2,12 @@ class UpdateDistroField < ContentfulMigrations::Migration
   def content_types
     %w(album article content_block category collection episode message meta page podcast redirect series song video)
   end
-  
+
   def up
     with_space do |space|
       content_types.each do |name|
         content_type = space.content_types.find(name)
-        
+
         widget_id = {
           'development' => '7pen61ICvT0RvmFXRepZa3',
           'int' => '2XO3c2aXJj0MDelzxDwB3f',
@@ -17,11 +17,13 @@ class UpdateDistroField < ContentfulMigrations::Migration
 
         editor_interface = content_type.editor_interface.default
         controls = editor_interface.controls
-        controls.detect { |c| c['fieldId'] == 'distribution_channels' }['widgetNamespace'] = 'extension'
-        controls.detect { |c| c['fieldId'] == 'distribution_channels' }['widgetId'] = widget_id
-        editor_interface.update(controls: controls)
-        editor_interface.reload
-
+        field = controls.detect { |c| c['fieldId'] == 'distribution_channels' }
+        if field
+          field['widgetNamespace'] = 'extension'
+          field['widgetId'] = widget_id
+          editor_interface.update(controls: controls)
+          editor_interface.reload
+        end
       end
     end
   end
@@ -30,13 +32,16 @@ class UpdateDistroField < ContentfulMigrations::Migration
     with_space do |space|
       content_types.each do |name|
         content_type = space.content_types.find(name)
-  
+
         editor_interface = content_type.editor_interface.default
         controls = editor_interface.controls
-        controls.detect { |c| c['fieldId'] == 'distribution_channel' }['widgetNamespace'] = 'builtin'
-        controls.detect { |c| c['fieldId'] == 'distribution_channel' }['widgetId'] = 'objectEditor'
-        editor_interface.update(controls: controls)
-        editor_interface.reload
+        field = controls.detect { |c| c['fieldId'] == 'distribution_channels' }
+        if field
+          field['widgetNamespace'] = 'builtin'
+          field['widgetId'] = 'objectEditor'
+          editor_interface.update(controls: controls)
+          editor_interface.reload
+        end
       end
     end
   end
