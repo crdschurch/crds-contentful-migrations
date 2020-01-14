b1b3f69e4463ada759916a918fdb41e622b24361
jakecatron <jake.catron@crossroads.net>
2020-01-14 11:41:16 -0500

    insert at the front of the array

--- migrations/20200114101622_add_apppalachia_trip.rb
+++ migrations/20200114101622_add_apppalachia_trip.rb
@@ -8,7 +8,7 @@ class AddApppalachiaTrip < ContentfulMigrations::Migration
       field = content_type.fields.detect { |f| f.id =='country'}
       validations = field.validations.first.in
       new_trip = 'Appalachia'
-      validations.push(new_trip)
+      validations.unshift(new_trip)
 
       content_type.save
       content_type.publish
