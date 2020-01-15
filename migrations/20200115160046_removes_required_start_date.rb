7b9418e589e584069b11621cf8e80997eb3b65e7
Josh Byron <joshhbyron@gmail.com>
2020-01-15 16:08:03 -0500

    feature/US18852-remove-start-date-requirement

---
+++ migrations/20200115160046_removes_required_start_date.rb
@@ -0,0 +1,26 @@
+class RemovesRequiredStartDate < ContentfulMigrations::Migration
+  def up
+    with_space do |space|
+
+      content_type = space.content_types.find('onsite_group_meeting')
+      field = content_type.fields.detect { |f| f.id == 'starts_at' } 
+      field.required = false
+
+      content_type.save
+      content_type.publish
+    end
+  end
+
+  def down
+    with_space do |space|
+
+      content_type = space.content_types.find('onsite_group_meeting')
+      field = content_type.fields.detect { |f| f.id == 'starts_at' } 
+      field.required = true
+
+      content_type.save
+      content_type.publish
+
+    end
+  end
+end
