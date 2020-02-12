651839d07a3477d2280999cae5641580a2ca278f
jakecatron <jake.catron@crossroads.net>
2020-02-12 14:17:53 -0500

    edits..

--- migrations/20200212135250_create_trip_itinerary.rb
+++ migrations/20200212135250_create_trip_itinerary.rb
@@ -4,15 +4,14 @@ class CreateTripItinerary < RevertableMigration
 
   def up
     with_space do |space|
-
       # Create content model
       content_type = space.content_types.create(
-        name: 'Trip_Itinerary',
+        name: 'Trip Itinerary',
         id: content_type_id,
         description: 'Itinerary for Go Trips', 
         displayField: 'day'
       )
-      
+
       # Create fields
       content_type.fields.create(id: 'day', name: 'Day', type: 'Symbol', required: true)
       content_type.fields.create(id: 'activities', name: 'Activities', type: 'Symbol', required: true)
