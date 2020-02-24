49564c1644620e9845029e189d3b6c63f8ada960
it <ll>
2020-02-24 10:09:35 -0500

    removing migration utils?

--- migrations/20200218084614_add_care_link_to_location.rb
+++ migrations/20200218084614_add_care_link_to_location.rb
@@ -1,7 +1,5 @@
 class AddCareLinkToLocation < ContentfulMigrations::Migration
 
-  include MigrationUtils
-
   def up
     with_space do |space|
       content_type = space.content_types.find('location')
