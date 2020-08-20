0e864a5dd9c3c02244fed775f50e96f8f8ebcd01
Taylor C. MacDonald <taylor@helloample.com>
2020-08-20 12:52:36 -0400

    US20052 Locations Fix exception related to name via generator

--- migrations/20200820124255_add_visibility_flag_to_locations.rb
+++ migrations/20200820124255_add_visibility_flag_to_locations.rb
@@ -1,4 +1,4 @@
-class AddVisibilityFlagToLocation < ContentfulMigrations::Migration
+class AddVisibilityFlagToLocations < ContentfulMigrations::Migration
   def up
     with_space do |space|
       content_type = space.content_types.find('location')
