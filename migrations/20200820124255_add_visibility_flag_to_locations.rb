37666996717a80995487c7d3b06f1fee20e766c8
Taylor C. MacDonald <taylor@helloample.com>
2020-08-20 12:45:21 -0400

    US20052 Locations Add flag to location model

---
+++ migrations/20200820124255_add_visibility_flag_to_locations.rb
@@ -0,0 +1,23 @@
+class AddVisibilityFlagToLocation < ContentfulMigrations::Migration
+  def up
+    with_space do |space|
+      content_type = space.content_types.find('location')
+      content_type.fields.create(id: 'is_visible', name: 'Visible on locations page?', type: 'Boolean')
+      content_type.save
+      content_type.publish
+    end
+  end
+
+  def down
+    with_space do |space|
+      content_type = space.content_types.find('location')
+      field = content_type.fields.detect { |f| f.id == 'is_visible' }
+      field.omitted = true
+      field.disabled = true
+
+      content_type.save
+      content_type.activate
+      content_type.fields.destroy('is_visible')
+    end
+  end
+end
