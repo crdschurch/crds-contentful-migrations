42841b3588a85e2e78bc20cd13a1d8abb8339001
Kell G <kell.gauthier@gmail.com>
2020-09-09 09:57:58 -0400

    Resolves class name

--- migrations/20200904154141_add_publisher_to_albums.rb
+++ migrations/20200904154141_add_publisher_to_albums.rb
@@ -1,4 +1,4 @@
-class AddPublisherToAlbum < ContentfulMigrations::Migration
+class AddPublisherToAlbums < ContentfulMigrations::Migration
   def up
     with_space do |space|
       content_type = space.content_types.find('album')
